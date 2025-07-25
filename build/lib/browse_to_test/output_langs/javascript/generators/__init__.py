e3e651230dcae3c6d86acea4f037860d87aabc2f
Quinn <quinnosha@gmail.com>
2025-07-24 18:57:28 -0700

    remov build artifacts

--- build/lib/browse_to_test/output_langs/javascript/generators/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-JavaScript code generators for different testing frameworks.
-"""
-
-from .playwright_generator import PlaywrightJavascriptGenerator
-
-__all__ = [
-    'PlaywrightJavascriptGenerator'
-] 
--- build/lib/browse_to_test/output_langs/typescript/generators/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TypeScript code generators for different testing frameworks.
-"""
-
-from .playwright_generator import PlaywrightTypescriptGenerator
-
-__all__ = [
-    'PlaywrightTypescriptGenerator'
-] 
